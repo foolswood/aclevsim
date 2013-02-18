7c362cabd6b34607ceff62d1b1ee71673ea815d4
David Honour <david@foolswood.co.uk>
2013-02-18 10:13:15 +0000

    Fortran, now without segfault.

--- src/matrix/gen_tm.f95
+++ src/matrix/gen_tm.f95
@@ -3,42 +3,34 @@ PROGRAM tm_test
         IMPLICIT NONE
 
         INTERFACE
-                FUNCTION gen_tm(a,b,k) RESULT(tm)
+                SUBROUTINE gen_tm(a,b,k,tm)
                         REAL, INTENT(IN) :: a(:,:), b(:,:), k
-                        COMPLEX, ALLOCATABLE :: tm(:,:)
-                END FUNCTION gen_tm
+                        COMPLEX, ALLOCATABLE, INTENT(OUT) :: tm(:,:)
+                END SUBROUTINE gen_tm
         END INTERFACE
         
         REAL, DIMENSION(4,2) :: a = 1, b = 3
         COMPLEX, ALLOCATABLE :: tm(:,:)
         
         PRINT *, size(a,1)
-        tm = gen_tm(a,b,3.0)
-
-        !DEALLOCATE(tm)
-
+        CALL gen_tm(a,b,3.0,tm)
+        PRINT *, size(tm,1), size(tm,2)
+        DEALLOCATE(tm)
 END PROGRAM tm_test
 
 
-FUNCTION gen_tm(a, b, k) RESULT(tm)
+SUBROUTINE gen_tm(a, b, k, tm)
         IMPLICIT NONE
         REAL, INTENT(IN) :: k
         REAL, INTENT(IN) :: a(:,:), b(:,:)
-        COMPLEX, ALLOCATABLE :: tm(:,:)
+        COMPLEX, ALLOCATABLE, INTENT(OUT) :: tm(:,:)
         INTEGER :: i, j
-        !REAL :: r
-        print *, ALLOCATED(tm)
+        REAL :: r
         ALLOCATE(tm(SIZE(a,2),SIZE(b,2)))
-        PRINT *, "tm size is:"
-        PRINT *, SIZE(tm,1), SIZE(tm,2)
-        PRINT *, "I", "J"
-        print *, ALLOCATED(tm)
         DO j = 1, SIZE(a,2)
                 DO i = 1, SIZE(b,2)
-                        PRINT *, i, j
-                        tm(i,j) = 1.0
-                        !gen_tm(i,j) = a(4,i) * EXP(CMPLX(0,-k*r)) / r
+                        r = SQRT(SUM((a(1:3,i) - b(1:3, j))*(a(1:3,i) - b(1:3, j)))) !USE NORM2?
+                        tm(i,j) = a(4,i) * EXP(CMPLX(0,-k*r)) / r
                 END DO
         END DO
-        PRINT *, "MADE IT!"
-END FUNCTION gen_tm
+END SUBROUTINE gen_tm
